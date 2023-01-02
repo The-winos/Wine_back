f26767f6c0c924d587fb3da3c3678b92c9ba4460
Jessy <japarker0421@gmail.com>
2023-01-02 13:48:57 -0700

    first commit

---
+++ api/utils.js
@@ -0,0 +1,26 @@
+
+function requireUser(req, res, next) {
+  if (!req.user) {
+    next({
+      error: "401",
+      name: "MissingUserError",
+      message: "You must be logged in to perform this action",
+    });
+  }
+  next();
+}
+
+function requireAdmin(req, res, next) {
+  if (!req.user.admin) {
+    next({
+      name: "Permissions Invalid ",
+      message: "You must have administrator access",
+    });
+  }
+  next();
+}
+
+module.exports = {
+  requireUser,
+  requireAdmin,
+};
