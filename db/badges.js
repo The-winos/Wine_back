253673054c352f51ff45e3ba82beb0ed7f995120
Jessy <japarker0421@gmail.com>
2023-01-11 13:46:37 -0700

    put in and finished badge table. Starting wine table

---
+++ db/badges.js
@@ -0,0 +1,100 @@
+const { client } = require("./client");
+
+async function createBadges(user_id){
+try {
+  const{
+    rows:[badges],
+  }= await client.query(`
+  INSERT INTO badges(author_id)
+  VALUES($1)
+  RETURNING *;
+  `,
+  [user_id]
+  );
+  return badges;
+
+} catch (error) {
+  throw error;
+}
+}
+
+async function getAllBadges(){
+  try {
+    const {rows}= await client.query(`
+    SELECT badges.*,
+    users.username AS author_id
+    FROM badges
+    JOIN users ON users.id = badges.author_id
+    `);
+    const badges= rows;
+    return badges;
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getBadgeById(id){
+try {
+  const {
+    rows: [badges],
+  }= await client.query(`
+  SELECT *
+  FROM badges
+  WHERE id=${id}
+  `);
+  return badges;
+} catch (error) {
+  throw error;
+}
+}
+
+async function getBadgeByUser({username}){
+  try {
+    const{
+      rows: [badges],
+    }=await client.query(`
+    SELECT badges.*, users.username AS user_username
+    FROM badges
+    JOIN badges on users.id = badges.author_id
+    WHERE username = $1
+    `, [username]);
+    const diffBadges= Promise.all(badges.map((badge)=>{
+      return attachBadgeToUser(badge)
+    }))
+    // make function attachBadgeToUser
+    return diffBadges;
+  } catch (error) {
+    throw error
+  }
+}
+
+async function updateBadge({id, fields={}}){
+  const setString =Object.keys(fields).map((key,index)=>
+  `"${key}"=$${index +1}`).join(", ");
+  if (setString.length === 0){
+    return;
+  }
+  try {
+    const {rows: [badge],}= await client.query(`
+    UPDATE badges
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+    `,
+    Object.values(fields));
+    return badge;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+
+module.exports = {
+createBadges,
+getAllBadges,
+getBadgeById,
+getBadgeByUser,
+updateBadge,
+};
